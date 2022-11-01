fa8d95423a9bbebc8b0749c142b5dd6c07dbb00c
Vadim Vladimirov <kurtonchik@gmail.com>
2022-11-01 18:32:36 +0200

    clear default settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
@@ -24,7 +22,7 @@ import { UsersModule } from './modules/users/users.module';
     ConfigModule.forRoot(),
     UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
